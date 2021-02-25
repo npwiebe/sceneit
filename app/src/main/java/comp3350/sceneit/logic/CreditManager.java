5daa455df3eaad6ea5f05bcb5f949803f71e4b1a
TimA <appleyat@myumanitoba.ca>
2021-02-24 19:50:24 -0600

    Creates UI for Credit Card Purchase Feature

---
+++ app/src/main/java/comp3350/sceneit/logic/CreditManager.java
@@ -0,0 +1,99 @@
+package comp3350.sceneit.logic;
+
+import java.util.Calendar;
+
+public class CreditManager {
+    //Checks if a string has any data in it.
+    static public boolean fieldFilled(String data){
+        return data != null && data.length() != 0;
+    }
+
+    //checks if a the date given in mmyy format (it assumes its that format) is after the currect date
+    //This is to check the expiry date on credit cards.
+    static public boolean checkDate(String date){
+        boolean validated = false;
+        int inputMonth;
+        int inputYear;
+        int dateNum;
+        Calendar c = Calendar.getInstance();
+        if(isNumeric(date) && date.length() == 4){
+            dateNum = Integer.parseInt(date);
+            inputMonth = dateNum/100; //removes the year digits
+            inputYear = dateNum%100; //Removes everything that isnt the last two digits
+            //the date is correct if the input month and year are less or equal to current month and year.
+            validated = (inputMonth >= c.get(Calendar.MONTH) && inputYear >= c.get(Calendar.YEAR)%100);
+        }
+        return validated;
+    }
+
+    //Uses Luhns algorithm to validate credit cards.
+    static public boolean validateCredit(String creditString){
+        boolean validated = false;
+        long workingOn;
+        long creditNum;
+        //Make sure the string passed in is numeric
+        if(isNumeric(creditString)) {
+            //Get a number from the string
+            creditNum = Long.parseLong(creditString);
+            //Check if credit card has 16 digits
+            if (String.valueOf(creditNum).length() == 16) {
+                //the next 4 lines basically all do Luhns algorithm for validating credit cards
+                workingOn = creditNum/10;//remove last digit from number
+                workingOn = reverseNumber(workingOn);
+                workingOn = Luhn(workingOn);
+
+                //
+                validated = ((workingOn + creditNum % 10) % 10 == 0);
+            }
+        }
+        return validated;
+    }
+
+    //Is this string able to be converted to a number.
+    static public boolean isNumeric(String number){
+        boolean isNumber;
+        try{
+            Long.parseLong(number);
+            isNumber = true;
+        } catch(NumberFormatException e){
+            isNumber =false;
+        }
+        return isNumber;
+    }
+
+    //Reverse what ever number is passed to it
+    static public long reverseNumber(long number){
+
+        long reversed = 0;
+        long storage = 0;
+
+        while(number != 0){
+            storage = number%10;
+            reversed = reversed * 10 + storage;
+            number /= 10;
+        }
+
+        return reversed;
+    }
+
+    //Luhn is all the super weird math parts of Luhns algorithm, look it up on wikipedia
+    static private long Luhn(long number){
+        //I need to convert my number to a string so i can look at specific digits on the number
+        String numberString = Long.toString(number);
+        long[] trackNumbers = new long[numberString.length()];
+        long total = 0;
+        //Go through number by number and do the doubling to every odd number and subtract 9 if that
+        //doubled number is greater then 9. Then add all them to total
+        for(int i = 0; i < numberString.length(); i++){
+            trackNumbers[i] = Character.getNumericValue(numberString.charAt(i));
+            if(i%2 == 0) {
+                trackNumbers[i] *= 2;
+                if (trackNumbers[i] > 9)
+                    trackNumbers[i] -= 9;
+            }
+            total += trackNumbers[i];
+        }
+
+        return total;
+     }
+}
---
+++ app/src/main/java/comp3350/sceneit/presentation/CreditActivity.java
@@ -0,0 +1,111 @@
+package comp3350.sceneit.presentation;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import comp3350.sceneit.R;
+import comp3350.sceneit.logic.CreditManager;
+
+public class CreditActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private String message;
+    private EditText nameCard, numberCard, cvc, expDate, country, province,
+            addressOne, city, postalCode, telephoneNumber, email;
+    private TextView purchaseDisplay;
+    private Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit);
+
+        //initialize buttons, text views, and edit texts
+        //All the EditTexts are named the same as their ids
+        purchaseDisplay = (TextView) findViewById(R.id.puchaseInfo);
+        submitButton = (Button) findViewById(R.id.button);
+        nameCard = (EditText) findViewById(R.id.nameCard);
+        numberCard = (EditText) findViewById(R.id.numberCard);
+        cvc = (EditText) findViewById(R.id.cvc);
+        expDate = (EditText) findViewById(R.id.expDate);
+        country = (EditText) findViewById(R.id.country);
+        province = (EditText) findViewById(R.id.province);
+        addressOne = (EditText) findViewById(R.id.addressOne);
+        city = (EditText) findViewById(R.id.city);
+        postalCode = (EditText) findViewById(R.id.postalCode);
+        telephoneNumber = (EditText) findViewById(R.id.telephoneNumber);
+        email = (EditText) findViewById(R.id.email);
+
+        //get stuff from the bundle that was passed
+        Bundle b = getIntent().getExtras();
+        int price = b.getInt("price");
+        int ticketsNum = b.getInt("ticketsNum");
+        String movieTitle = b.getString("movieTitle");
+        String theatre = b.getString("theatre");
+
+        message = "Purchasing " + ticketsNum + " Tickets for " + movieTitle + " at " + theatre + " for $" + price;
+        purchaseDisplay.setText(message);
+
+        submitButton.setOnClickListener(this);
+
+    }
+
+    public void callAlert(String message){
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+        //set title
+        .setTitle("Error")//Tell the user theres an error in the data
+        //set message
+        .setMessage(message)//setup with the message that gets passed
+        //set the okay button for the user.
+         .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+             //this does nothing but the ok button needs it to work correctly so it exists.
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+         })
+         .show();//show the alert on screen
+    }
+
+    @Override
+    public void onClick(View v){
+        //This is a lot of statements to go through each field I know
+        //Couldnt figure out a way to go through each field and also have a unique message.
+        if(!CreditManager.validateCredit(numberCard.getText().toString())) {
+            callAlert("Invalid Credit Card Number");
+        } else if(!CreditManager.fieldFilled(nameCard.getText().toString())){
+            callAlert("Please fill in the name located on your Credit card");
+        } else if(!CreditManager.fieldFilled(cvc.getText().toString())){
+            callAlert("Please fill in the cvc located on your Credit card");
+        } else if(!CreditManager.checkDate(expDate.getText().toString())){
+            callAlert("This card may be expired, check your expiry date");
+        } else if(!CreditManager.fieldFilled(country.getText().toString())){
+            callAlert("Please fill in your country");
+        } else if(!CreditManager.fieldFilled(province.getText().toString())){
+            callAlert("Please fill in your Region/Province/State");
+        } else if(!CreditManager.fieldFilled(addressOne.getText().toString())){
+            callAlert("Please fill in your Address One");
+        } else if(!CreditManager.fieldFilled(cvc.getText().toString())){
+            callAlert("Please fill in the cvc located on your Credit card");
+        } else if(!CreditManager.fieldFilled(cvc.getText().toString())){
+            callAlert("Please fill in your city");
+        } else if(!CreditManager.fieldFilled(postalCode.getText().toString())){
+            callAlert("Please fill in your Postal Code");
+        } else if(!CreditManager.fieldFilled(telephoneNumber.getText().toString())){
+            callAlert("Please fill in your Telephone");
+        } else if(!CreditManager.fieldFilled(email.getText().toString())){
+            callAlert("Please fill in your email");
+        } else{
+            //This is here for when we link to the next activity. Like confirmation page
+            //Intent intent = new Intent(this, CreditActivity.class);
+            //startActivity(intent);
+        }
+    }
+}
