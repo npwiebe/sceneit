68188368fe99864312e7473a8ff48872bc583be0
Jacob Linney <jacob@linney.dev>
2021-03-11 13:45:52 -0600

    Add User Class

--- app/src/main/java/comp3350/sceneit/data/UserManager.java
+++ app/src/main/java/comp3350/sceneit/data/UserManager.java
@@ -10,18 +10,18 @@ public interface UserManager {
      * @param username The username to use
      * @param password The password to use
      * @param name The users name, Ex. Joe Smoe
-     * @return A JWT of the logged in user info.
+     * @return The logged in users info
      * @throws UserExistsException if the user already exists
      */
-    public String register(String username, String password, String name) throws UserExistsException;
+    public User register(String username, String password, String name) throws UserExistsException;
 
     /**
      * Login as a user
      *
      * @param username The username to use
      * @param password The password to use
-     * @return A JWT of the logged in user info.
+     * @return The logged in users info
      * @throws InvalidUserCredentials if the user could not be logged in due to invalid credentials
      */
-    public String login(String username, String password) throws InvalidUserCredentials;
+    public User login(String username, String password) throws InvalidUserCredentials;
 }
