c78bbec8c4dd71806a520c5fc59abd5b2433e80f
Jacob Linney <jacob@linney.dev>
2021-03-18 13:50:30 -0500

    UserManager: Initial Implementation
    - Does not throw UserNotExist/Login Failures

--- app/src/main/java/comp3350/sceneit/data/User.java
+++ app/src/main/java/comp3350/sceneit/data/User.java
@@ -3,10 +3,16 @@ package comp3350.sceneit.data;
 public class User {
     private String username;
     private String name;
+    private String email;
 
-    public User(String username, String name) {
+    public User(String username, String name, String email) {
         this.username = username;
         this.name = name;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public String getUsername() {
